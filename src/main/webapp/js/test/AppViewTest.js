556d3b929a5f7c9e1d8fbdd03897676f736795fb
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-03-26 12:40:48 +0200

    Fix AppViewTest.js

--- src/main/webapp/js/test/AppViewTest.js
+++ src/main/webapp/js/test/AppViewTest.js
@@ -1,13 +1,15 @@
 var assert = require('assert');
-var main = require('../main.js');
-
-//requirejs
+var requirejs = require('requirejs');
 
 describe('AppView', function() {
 	describe('views', function() {
-		it('should contain three views', function() {
-//			var app_view = new testand.AppView;
-//			assert.equal(app_view.views.length == 3);
+		it('should contain three elements (all three defined)', function() {
+			requirejs(['../main.js'], function(main) {
+				var app_view = new main.AppView;
+				assert.equal(app_view.views.length == 3);
+				for (var i = 0; i < app_view.views.length; i++)
+					assert.equal(app_view.views[i] != undefined);
+			});
 		});
 	});
 });
