d16ce234b4ecc748755202c505b8b7997ecbdd08
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-03-26 12:00:49 +0200

    Unit testing problem

---
+++ src/main/webapp/js/test/AppViewTest.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var main = require('../main.js');
+
+//requirejs
+
+describe('AppView', function() {
+	describe('views', function() {
+		it('should contain three views', function() {
+//			var app_view = new testand.AppView;
+//			assert.equal(app_view.views.length == 3);
+		});
+	});
+});
--- src/main/webapp/js/views/AppView.js
+++ src/main/webapp/js/views/AppView.js
@@ -11,20 +11,19 @@ define([
     'views/detailsview',
     'views/newcellview'
 
-], function ($, _, Backbone, joint, ProjectView, Command, DetailsView, newCellView) {
+], function ($, _, Backbone, joint, ProjectView, Command, DetailsView, NewCellView) {
     var AppView = Backbone.View.extend({
         el: $('#navbar'),
-        op: {
-            pv: {},
-            dv: {},
-            nv: {},
-            ind: 0
+        views: {
+            project: {},
+            details: {},
+            newCell: {},
+            index: 0
         },
         initialize: function () {
-            this.op.pv = ProjectView;
-            this.op.dv = new DetailsView;
-            this.op.nv = new newCellView;
-
+            this.views.project = ProjectView;
+            this.views.details = new DetailsView;
+            this.views.newCell = new NewCellView;
         },
         events: {
             'click #switch': 'switchgraph',
@@ -32,17 +31,16 @@ define([
             'change #files': 'load'
         },
         switchgraph: function () {
-            if (this.op.ind == 0) {
-                this.op.pv.switch(1);
-                this.op.ind = 1;
+            if (this.views.index == 0) {
+                this.views.project.switch(1);
+                this.views.index = 1;
             }
             else {
-                this.op.pv.switch(0);
-                this.op.ind = 0;
+                this.views.project.switch(0);
+                this.views.index = 0;
             }
         },
         save: function (event) {
-
             Command.execute("saveDiagram");
         },
         load: function (event) {
