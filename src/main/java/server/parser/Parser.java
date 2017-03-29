0ae301f73641015e69a2fc391552d9dd0d1543ad
alberto zanatta <alberto zanatta>
2017-03-29 13:19:01 +0200

    modifica Parser.java e prova.json

--- src/main/java/server/parser/Parser.java
+++ src/main/java/server/parser/Parser.java
@@ -1,17 +1,40 @@
 package server.parser;
 
 import org.json.*;
+
+import server.project.ParsedAttribute;
+import server.project.ParsedClass;
 import server.project.ParsedProgram;
 
 public class Parser {/*abstract???anche i metodi sono abstract*/
 	public static ParsedProgram createParsedProgram(String jsonstring) throws JSONException{
 		
-		JSONObject Programma = new JSONObject(jsonstring);
+		JSONArray arr = new JSONObject(jsonstring).getJSONObject("classes").getJSONArray("cells");
+		JSONObject generalclass = arr.getJSONObject(0).getJSONObject("values");
+		JSONArray attributes = generalclass.getJSONArray("attributes");
 		
-		JSONArray arr = Programma.getJSONArray("classes");
+		/*String cells = Programma.getString("cells");
+		JSONObject tutte_classi = new JSONObject(cells);
 		
-		for (int i = 0; i < arr.length(); i++)
-		  System.out.println(arr.getJSONObject(i).getString("id"));
+		JSONArray arr = tutte_classi.getJSONArray("cells");*/
+		ParsedAttribute[] fields = new ParsedAttribute[attributes.length()];
+		
+		for (int i = 0; i < attributes.length(); i++){
+			JSONObject attr = attributes.getJSONObject(i);
+			String visibility = attr.getString("varvisib");
+			String name = attr.getString("varname");
+			String type = attr.getString("vartype");
+			String value = null;
+			if(attr.has("varvalue"))
+				value = attr.getString("varvalue");
+			ParsedAttribute att = new ParsedAttribute(false, visibility, type, name, value);
+			fields[i] = att;
+		}
+		
+		String class
+		
+		ParsedClass pclass = new ParsedClass();
+		  //System.out.println(values.getJSONObject(i).getString("id"));
 		
 		return new ParsedProgram();};
 	public void saveToDisk(String IdReq){};
