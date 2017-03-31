6bbf60a8077c8b7b1768a1356670bdf9b3b4c341
BaraccoPaolo <paolo.baracco.1@studenti.unipd.it>
2017-03-31 15:48:18 +0200

    fatto nel ProvaMain un prova di un file zip contenente il file json cambiando il nome in px.json funzionante

--- src/main/java/server/ProvaMain.java
+++ src/main/java/server/ProvaMain.java
@@ -1,10 +1,15 @@
 package server;
 
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 
 import org.json.JSONException;
 import org.springframework.context.ApplicationContext;
@@ -40,6 +45,31 @@ public class ProvaMain {//SIMULO IL PROGRAMMA PRINCIPALE
 		ApplicationContext context = new AnnotationConfigApplicationContext(GeneratorAssembler.class);
 		RequestHandlerController rhc = (RequestHandlerController)context.getBean("rhc");
 		rhc.prova(provv);
+		
+		/*parte zip*/
+		byte[] buffer = new byte[1024];
+		
+		FileOutputStream fos = new FileOutputStream("src/main/resources/prova.zip");/*In Java, FileOutputStream is a bytes 
+		stream class that’s used to handle raw binary data. To write the data to file, you have to convert the data into 
+		bytes and save it to file*//*crea il zip vuoto*/
+		ZipOutputStream zos = new ZipOutputStream(fos);/*This class implements an output stream filter for writing files 
+		in the ZIP file format*/
+		ZipEntry ze= new ZipEntry("px"); /*This class is used to represent a ZIP file entry*//*da il nome al file dentro
+		lo zip*/
+		zos.putNextEntry(ze);/*This class is used to represent a ZIP file entry.*/
+		FileInputStream in = new FileInputStream("src/main/resources/prova.json");
+		/*A FileInputStream obtains input bytes from a file in a file system*/
+		
+		int len;
+		while ((len = in.read(buffer)) > 0) {/*	read(byte[] b) Reads up to b.length bytes of data from this input 
+		stream into an array of bytes.*/
+			zos.write(buffer, 0, len);/*write(byte[] b, int off, int len)*/
+		}
+		
+		in.close();
+		zos.closeEntry();
+		zos.close();/*importante chiudere!!!*/
+		
 	}
 
 }
