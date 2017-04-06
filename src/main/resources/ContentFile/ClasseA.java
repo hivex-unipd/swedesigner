cd226cf7324843261b53290b02cbe2b7ab6c5232
alberto zanatta <alberto zanatta>
2017-04-06 11:47:22 +0200

    pulizia generale del server: cancellato package check e classi all'interno, cancellata classe ParsedRelationship.java, cancellati template relativi a assegnazione, inizializzazione e variabile

--- src/main/resources/ContentFile/ClasseA.java
+++
@@ -1,5 +0,0 @@
-public class ClasseA{
-int var3;
-bool var4;
-public tipoRitorno metodo2(int param1);
-};
--- src/main/resources/ContentFile/ClasseB.java
+++
@@ -1,8 +0,0 @@
-public class ClasseB extends ClasseA{
-int var1;
-float var2;
-public tipoRitorno metodo1(String pippo = 'paperino') {
-for (;;) {else{
-}}
-}
-};
