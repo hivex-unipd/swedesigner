d567a0557f328ec899335067421557ece5e9e310
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-04-08 10:34:15 +0200

    documentaz. test

--- src/test/java/project/ParsedAttributeTest.java
+++ src/test/java/project/ParsedAttributeTest.java
@@ -8,9 +8,11 @@ import server.project.ParsedAttribute;
 import server.template.java.JavaTemplate;
 
 public class ParsedAttributeTest {
+
+	// Costruito un ParsedAttribute, questo è in grado di generare una stringa contenente tipo e nome passatigli nel costruttore.
 	@Test
 	public void attributeContainsTypeAndName() {
-		ParsedAttribute attribute = new ParsedAttribute(false, "private", "String", "pippo", "", "");
+		ParsedAttribute attribute = new ParsedAttribute(false, "private", "String", "pippo", null, null);
 		JavaTemplate template = new JavaTemplate();
 		String result = attribute.renderTemplate(template);
 		assertThat(result, containsString("String pippo"));
