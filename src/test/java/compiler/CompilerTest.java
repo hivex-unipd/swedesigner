aabed249db5307223b1d6f05601a53f16e80c25e
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-04-25 17:57:36 +0200

    fix test

--- src/test/java/compiler/CompilerTest.java
+++ src/test/java/compiler/CompilerTest.java
@@ -31,7 +31,7 @@ public class CompilerTest {
 		assertEquals(0, results.size());
 	}
 
-	// un JavaCompiler a cui venga chiesto di compilare i file di una cartella inesistente ritorna una lista di errori contenente l'errore "[cartella] is not a valid directory".
+	// Un JavaCompiler a cui venga chiesto di compilare i file di una cartella inesistente ritorna una lista di errori contenente l'errore "[cartella] is not a valid directory".
 	@Test
 	public void compilerRejectsNonDirectories() throws IOException {
 		List<String> results = compiler.compile("src/main/resources/nothing");
