e10c6529e46f53376c6266f3899a4a64c91805ff
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-05-07 09:14:03 +0200

    javadoc per Template e JavaTemplate

--- src/main/java/server/template/Template.java
+++ src/main/java/server/template/Template.java
@@ -9,14 +9,70 @@ import org.stringtemplate.v4.ST;
  * a source code string.
  */
 public interface Template {
+
+	/**
+	 * Returns a ST template for generating class
+	 * members (attributes).
+	 * @return a template for attributes
+	 */
 	ST getAttributeTemplate();
+
+	/**
+	 * Returns a ST template for generating "for"
+	 * loops.
+	 * @return a template for "for" loops
+	 */
 	ST getForTemplate();
+
+	/**
+	 * Returns a ST template for generating 
+	 * conditional statements.
+	 * @return a template for conditional statements
+	 */
 	ST getIfTemplate();
+
+	/**
+	 * Returns a ST template for generating return
+	 * statements
+	 * @return a template for return statements
+	 */
 	ST getReturnTemplate();
+
+	/**
+	 * Returns a ST template for generating classes.
+	 * @return a template for classes
+	 */
 	ST getClassTemplate();
+
+	/**
+	 * Returns a ST template for generating methods.
+	 * @return a template for methods
+	 */
 	ST getMethodTemplate();
+
+	/**
+	 * Returns a ST template for generating "while"
+	 * loops.
+	 * @return a template for "while" loops
+	 */
 	ST getWhileTemplate();
+
+	/**
+	 * Returns a ST template for generating interfaces.
+	 * @return a template for interfaces
+	 */
 	ST getInterfaceTemplate();
+
+	/**
+	 * Returns a ST template for generating alternative
+	 * conditional statements.
+	 * @return a template for alternative conditional statements
+	 */
 	ST getElseTemplate();
+
+	/**
+	 * Returns a ST template for generating statements.
+	 * @return a template for statements
+	 */
 	ST getStatementTemplate();
 }
--- src/main/java/server/template/java/JavaTemplate.java
+++ src/main/java/server/template/java/JavaTemplate.java
@@ -15,51 +15,97 @@ public class JavaTemplate implements Template {
 
 	private STGroup group = new STGroupDir("templates");
 
+	/**
+	 * Returns a ST template for generating Java class
+	 * members (attributes).
+	 * @return a template for attributes
+	 */
 	@Override
 	public ST getAttributeTemplate() {
 		return group.getInstanceOf("attributejavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java "for"
+	 * loops.
+	 * @return a template for "for" loops
+	 */
 	@Override
 	public ST getForTemplate() {
 		return group.getInstanceOf("forjavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java 
+	 * conditional statements.
+	 * @return a template for conditional statements
+	 */
 	@Override
 	public ST getIfTemplate() {
 		return group.getInstanceOf("ifjavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java return
+	 * statements
+	 * @return a template for return statements
+	 */
 	@Override
 	public ST getReturnTemplate() {
 		return group.getInstanceOf("returnjavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java classes.
+	 * @return a template for classes
+	 */
 	@Override
 	public ST getClassTemplate() {
 		return group.getInstanceOf("classjavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java methods.
+	 * @return a template for methods
+	 */
 	@Override
 	public ST getMethodTemplate() {
 		return group.getInstanceOf("methodjavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java "while"
+	 * loops.
+	 * @return a template for "while" loops
+	 */
 	@Override
 	public ST getWhileTemplate() {
 		return group.getInstanceOf("whilejavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java interfaces.
+	 * @return a template for interfaces
+	 */
 	@Override
 	public ST getInterfaceTemplate() {
 		return group.getInstanceOf("interfacejavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java alternative
+	 * conditional statements.
+	 * @return a template for alternative conditional statements
+	 */
 	@Override
 	public ST getElseTemplate() {
 		return group.getInstanceOf("elsejavatemplate");
 	}
 
+	/**
+	 * Returns a ST template for generating Java statements.
+	 * @return a template for statements
+	 */
 	@Override
 	public ST getStatementTemplate() {
 		return group.getInstanceOf("statementjavatemplate");
