622c1e4de59dad34e5d4519b9e0ca6daede73f54
oigroig <giorgio.giuffre@studenti.unipd.it>
2017-05-07 09:30:17 +0200

    javadoc per Stereotype

--- src/main/java/server/stereotype/Stereotype.java
+++ src/main/java/server/stereotype/Stereotype.java
@@ -1,12 +1,41 @@
 package server.stereotype;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * A {@code Stereotype} object represents a
  * custom kind of type for modeling a class
  * of types when designing a program.
  */
 public class Stereotype {
-	public String getMetaAttributes() {return null;}
-	public String getMetaMethods() {return null;}
-	public String Stereotype(String path) {return null;}
+
+	private List<String> metaAttributes;
+	private List<String> metaMethods;
+
+	/**
+	 * Constructor for a stereotype.
+	 * @param  path where the stereotype is located
+	 * @return      a new Stereotype
+	 */
+	public Stereotype(String path) {
+		metaAttributes = new ArrayList<String>();
+		metaMethods = new ArrayList<String>();
+	}
+
+	/**
+	 * Returns the meta-attributes of the stereotype.
+	 * @return the meta-attributes
+	 */
+	public String getMetaAttributes() {
+		return null;
+	}
+
+	/**
+	 * Returns the meta-methods of the stereotype.
+	 * @return the meta-methods
+	 */
+	public String getMetaMethods() {
+		return null;
+	}
 }
